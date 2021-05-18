cb283f7f0d5644c27ba1a944d30aa375669d8e22
PranaySinguluri <singuluripranay20@gmail.com>
2021-05-18 15:45:25 +0530

    v2

--- src/main/java/com/coronaconsultation/entities/Medicine.java
+++ src/main/java/com/coronaconsultation/entities/Medicine.java
@@ -9,19 +9,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
-import org.springframework.validation.annotation.Validated;
-
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import lombok.Data;
 
 @Entity
-@Validated
+
 @Data
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "medicine_id", scope = Integer.class)
 public class Medicine {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	
@@ -30,17 +28,17 @@ public class Medicine {
 	@Column(nullable= false)
 	private String name;
 	
-	//@Column(name="medicine_price")
+	
 	@Column(nullable= false)
 
 	private int price;
 	@Column(nullable= false)
-	//@Column(name="manufacture_date")
+
 	private LocalDate manufactureDate;
 	
 	@Column(nullable= false)
 
-	//@Column(name="expiry_date")
+	
 	private LocalDate expiryDate;
 	
 	@Column(nullable= false)
--- src/test/java/com/coronaconsultation/services/MedicineServiceTest.java
+++ src/test/java/com/coronaconsultation/services/MedicineServiceTest.java
@@ -1,11 +1,15 @@
 package com.coronaconsultation.services;
 
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import com.coronaconsultation.service.MedicineServiceImpl;
+import com.coronaconsultation.entities.Medicine;
 import com.coronaconsultation.repository.MedicineRepository;
 public class MedicineServiceTest {
 	@InjectMocks
@@ -17,6 +21,17 @@ public class MedicineServiceTest {
 	@BeforeAll
 	public void init() {
 		MockitoAnnotations.initMocks(this);
-
 }
+	@Test
+	 void getAllreports() {
+		Medicine med = new Medicine();
+				
+	}
+	@Test
+	public void getMedicineById() {
+		// when()
+	}
+//		when(dataServiceMock.retrieveAllData()).thenReturn(new int[] { 1, 2, 3 });
+//		assertEquals(6, business.calculateSumUsingDataService());
+
 }
